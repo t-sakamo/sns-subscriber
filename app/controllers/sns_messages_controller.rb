3eb2e08c0f99c6019fa22877ac943c881247d9ab
t-sakamo <git@nil.orz.ne.jp>
2020-03-04 09:26:57 +0000

    add post method sns message controller

--- app/controllers/sns_messages_controller.rb
+++ app/controllers/sns_messages_controller.rb
@@ -1,6 +1,11 @@
 class SnsMessagesController < ApplicationController
+
   def index
     logger.info("=== entering ===")
     @sns_messages = SnsMessage.all
+  end
+
+  def create
+    logger.info(request.headers)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'sns_messages#index'
 
-  resources :sns_messages, only: %s[index]
+  resources :sns_messages, only: %i[index create]
 end
