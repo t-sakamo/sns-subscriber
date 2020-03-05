c5252b24e19eb93f00909c862216584c98e880e4
t-sakamo <git@nil.orz.ne.jp>
2020-03-05 15:05:57 +0000

    add check confirm subscription

--- app/controllers/sns_messages_controller.rb
+++ app/controllers/sns_messages_controller.rb
@@ -9,10 +9,43 @@ class SnsMessagesController < ApplicationController
 
   def create
     logger.debug("=== body ===")
-    JSON.parse(request.body.read).tap do |body_params|
-      body_params.each {|k, v| logger.debug("#{k} : #{v}")}
-    end
+    body_params.each {|k, v| logger.debug("#{k} : #{v}")}
+
     logger.debug("=== header ===")
     request.headers.env.each {|k, v| logger.debug("#{k} : #{v}")}
+
+    case body_params["Type"]
+    when "SubscriptionConfirmation"
+      confirm_subscription
+    end
+  end
+
+  private
+
+  def request_body
+    @request_body ||= request.body.read
+  end
+
+  def body_params
+    @body_params ||= JSON.parse(request_body)
+  end
+
+  def confirm_subscription
+    return false unless check_confirm_subscription
+    do_confirm_subscription
+  end
+
+  def check_confirm_subscription
+    # TopicArnと突合
+    # 省略
+
+    # Signatureをチェックする
+    ret = Aws::SNS::MessageVerifier.new.authentic?(request_body)
+    logger.debug("verifier.authentic? is true") if ret
+    logger.debug("verifier.authentic? is false") unless ret
+    ret
+  end
+
+  def do_confirm_subscription
   end
 end
