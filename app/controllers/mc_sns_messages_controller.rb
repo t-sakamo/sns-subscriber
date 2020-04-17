d7a34deabb2aad7bf3ed3243c35429bc7eb7e738
t-sakamo <git@nil.orz.ne.jp>
2020-04-17 20:44:03 +0900

    add debug

--- app/controllers/mc_sns_messages_controller.rb
+++ app/controllers/mc_sns_messages_controller.rb
@@ -3,14 +3,9 @@ class McSnsMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    logger.info("### params ###")
-    params.each {|k, v| logger.debug("#{k} : #{v}")}
-    logger.info("##############")
+    logger.debug("request.body.read = #{request.body.read}")
     return head 204
 
-    logger.info("request_body: #{request_body}")
-#    logger.info("body_params: #{body_params}")
-    logger.info("body_params[Type]: #{body_params["Type"]}")
     case body_params["Type"].to_sym
     when :SubscriptionConfirmation
       confirm_subscription
